ded108a6d755dcd7c1c6af370666374958c3174b
Leopoldiev <leoflamefire777@gmail.com>
2025-03-20 10:30:28 +0300

    Done 02/01.js corrections

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,7 +13,7 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess=isAdmin && isVerifiedUser && (hasSpecialPermission || hasTemporaryPass)
+let isAccess=(isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass)
 console.log(isAccess);
 
 // your code
