3d996511fb4d95675d2d882fd1f6322e2ea1d3d0
Leopoldiev <leoflamefire777@gmail.com>
2025-03-28 13:50:32 +0300

    05/01 done

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -11,9 +11,19 @@
 */
 
 const game = {
-  resources: {
-    gold: 250,
-    lumber: 100,
-  },
-  addResource() {}
+   resources: {
+      gold: 250,
+      lumber: 100,
+   },
+   addResource(resource, amount) {
+      if (resource in this.resources) {
+         this.resources[resource] += amount;
+      }
+      else {
+         console.log("Invalid resource");
+      }
+   }
 }
+
+game.addResource("gold",100);
+console.log(game);
