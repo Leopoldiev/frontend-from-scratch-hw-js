2461fdc32b642a0ad9b60faad7539d13ba55c149
Leopoldiev <leoflamefire777@gmail.com>
2025-04-18 15:13:18 +0300

    02/07.js completed

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -4,7 +4,16 @@
  */
 
 function isNumeric(str) {
-  // your code
+   let countSpaces = 0;
+
+   for (let i = 0; i < str.length; i++) {
+      if (str[i] === " ") countSpaces++
+   }
+   if (countSpaces === str.length || str.length === 0) return false;
+
+   let result = +str;
+   if ((result <= 0 || result > 0) && typeof (result) === 'number') { return true; }
+   else return false;
 }
 
 // console.log(isNumeric("123")) // Ожидаемый результат: true
@@ -12,3 +21,5 @@ function isNumeric(str) {
 // console.log(isNumeric("123abc")) // Ожидаемый результат: false
 // console.log(isNumeric("abc")) // Ожидаемый результат: false
 // console.log(isNumeric(" ")) // Ожидаемый результат: false
+
+console.log(isNumeric(""));
